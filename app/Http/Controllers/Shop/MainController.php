c87b36695e356a2fd9823ff3ed54f8beb79220fa
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2022-03-16 22:10:34 +0100

    dd

--- app/Http/Controllers/Shop/MainController.php
+++ app/Http/Controllers/Shop/MainController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers\Shop;
 
-use App\Http\Controllers\Controller;
+use App\Models\Produit;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MainController extends Controller
 {
     public function index(){
 
-        return view('shop.index');
+        $produits = Produit::all();
+        
+
+        return view('shop.index', compact('produits'));
 
     }
 }
