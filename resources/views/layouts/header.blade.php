e4221ff29db2e7183dde491b82d0f278cd4ea7a5
Mehdi <mehdi.medimegh@isamm.u-manouba.tn>
2022-03-22 10:00:44 +0100

    comment hard code

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
           </a>
           <ul class="navbar-nav">
               <li class="nav-item">
-                  <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Panier</a>
+                  <a class="nav-link" href="{{ route('cart_index') }}"><i class="fa fa-shopping-cart"></i> Panier</a>
               </li>
           </ul>
       </div>
